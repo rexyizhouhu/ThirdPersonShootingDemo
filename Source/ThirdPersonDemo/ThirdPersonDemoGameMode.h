680b1bd9a169abe778246a48c75c03aac6e0681f
Rex <rexyizhouhu@gmail.com>
2021-11-09 02:06:51 +0800

    first commit

---
+++ Source/ThirdPersonDemo/ThirdPersonDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AThirdPersonDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AThirdPersonDemoGameMode();
+};
+
+
+
